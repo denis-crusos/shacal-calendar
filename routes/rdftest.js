804591b3709d82bc5fe68681d7fed9c9594994a3
denis-crusos <denisc_rusos@yahoo.com>
2015-01-24 20:12:55 +0200

    Added ontology and some queryes

--- routes/rdftest.js
+++ routes/rdftest.js
@@ -6,7 +6,11 @@ var router = express.Router();
 
 router.get("/", function(req, res) {
 	var endpoint = "http://localhost:3030/ds/query";
-	var query = "PREFIX ab: <http://learningsparql.com/ns/addressbook#>\nSELECT ?craigEmail\nWHERE\n{ ab:craig ab:email ?craigEmail . }\nLIMIT 10";
+	var query = "PREFIX ab: <http://learningsparql.com/ns/addressbook#>" +
+		"SELECT ?craigEmail" +
+		"WHERE" +
+		"{ ab:craig ab:email ?craigEmail . }" +
+		"LIMIT 10";
 	var client = new SparqlClient(endpoint);
 	client.query(query).execute(function(error, results) {
 		console.log(JSON.stringify(results, null, "\t"));
@@ -16,3 +20,17 @@ router.get("/", function(req, res) {
 });
 
 module.exports = router;
+
+/*
+
+	fuseki-server --update --loc=E:\Denis\FACULTATE\MSD2\DAW\FusekiDB /ds
+
+	PREFIX foaf: <http://xmlns.com/foaf/0.1/>
+SELECT ?name
+WHERE {
+ ?person foaf:mbox <mailto:alice@example.org> .
+ ?person foaf:name ?name .
+}
+
+fuseki-server --config=myTDBconfig.ttl
+*/
